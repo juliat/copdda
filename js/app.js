4c7bdd21f0a9cbf91bf3c6ae72f7881747f6fe1a
Julia Teitelbaum <julialt@gmail.com>
2014-04-21 22:14:33 -0400

    lotsa content.

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
      ascensorFloorName: [
      'intro',
      'whatIsCOPD',
+     'howTreatmentsWork',
      'whoIsThisFor',
      'whyAChoice',
      'noSurvivalBenefit',
