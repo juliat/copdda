7568f76854cacb0f65324eda43407920d721d2fd
Julia Teitelbaum <julialt@gmail.com>
2014-04-24 10:33:07 -0400

    moved some content and tweaked styles to try out two-column layout

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
      'intro',
      'whoIsThisFor',
      'whatIsCOPD',
+     'whatIsChronicBronchitis',
+     'whatIsEmphysema',
      'howTreatmentsWork',
      'whyAChoice',
      'noSurvivalBenefit',
