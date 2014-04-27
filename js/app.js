3b4a6752ddaf5cc9a1a711022e49e9b47e2765d6
Julia Teitelbaum <julialt@gmail.com>
2014-04-27 16:30:09 -0400

    figured out that this branch has most up to date content, so got ascensor working with it and going to merge with other branches

--- js/app.js
+++ js/app.js
@@ -18,7 +18,6 @@ $(document).ready(function() {
     });
   };
 
-/*
   var ascensor = $('#pagesWrapper').ascensor(
     {direction: 'x',
      height: '92%',
@@ -60,28 +59,23 @@ $(document).ready(function() {
     ascensorInstance.next();
   });
 
-  */
   // to update progress bar
   /* make the progress bar link to different content */
-  /*
   $(".progress li").click(function(event, index) {
     ascensorInstance.scrollToFloor($(this).index());
   });
-  */
   /* docs on the :eq function of jquery https://api.jquery.com/eq/ */
-  // $(".progress li:eq("+ ascensor.data("current-floor") +")").addClass("selected");
-/*
+  $(".progress li:eq("+ ascensor.data("current-floor") +")").addClass("selected");
+
   ascensor.on("scrollStart", function(event, floor){
     $(".progress li").removeClass("selected");
-    console.log(floor.to);
+    // console.log(floor.to);
     $(".progress li:eq("+floor.to+")").addClass("selected");
     setUpCollapsedParagraphs();
     loadVideoInstructions();
-*/
-
     /* tried simulating click on floor transition with no success */
     /* $("#floor-"+floor.to).click(); */
-//  });
+  });
 
   Mousetrap.bind('down', function() {
     console.log("down");
