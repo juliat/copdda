7acb80745be02b75c3b5c1be24ee70f78d9c5453
Julia Teitelbaum <julialt@gmail.com>
2014-05-05 18:59:00 -0400

    hardcoded footer into all pages. experimenting with header text. also tweaked a few CSS styles

--- js/fakingServer.js
+++ js/fakingServer.js
@@ -1,30 +1,17 @@
 $(window).load(function(){
-  function loadVideoInstructions() {
-    $(".vidInstructions").each(function(){
-      $(this).load("youtubePlay.html");
+
+  function setupPages() {
+    // bind footer links
+    $(".prev").on('click',function() {
+      ascensorInstance.prev();
     });
-  };
 
-  function addFooter() {
-    $('.floor').each(function(){
-      $(this).append("<div class='pageFooter'><a class='prev'>Click here to go to previous page</a><a class='next'>Click here to go to next page</a></div>");
+    $(".next").on('click',function() {
+      ascensorInstance.next();
     });
-  };
+  }
 
-function setupPages() {
-  addFooter();
-  loadVideoInstructions();
-
-  // bind footer links
-  $(".prev").on('click',function() {
-    ascensorInstance.prev();
-  });
-
-  $(".next").on('click',function() {
-    ascensorInstance.next();
-  });
-}
-
+  setupPages();
 });
 
 $(document).ready(function(){
